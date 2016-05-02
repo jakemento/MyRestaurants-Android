19066fdc85397c9e4e98a8d9004fe9c801d90aa4
Jake Steinberg <jakesteinberg@gmail.com>
2016-05-02 15:22:18 -0700

    update app to create a Value Event Listener

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.epicodus.myrestaurants.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,32 +10,79 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mfindRestaurants;
-    private EditText mLocationEditText;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private Firebase mSearchedLocationRef;
+    private ValueEventListener mSearchedLocationRefListener;
+
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.findRestaurants) Button mFindRestaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mfindRestaurants = (Button) findViewById(R.id.findRestaurants);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mfindRestaurants.setOnClickListener(new View.OnClickListener() {
+        mSearchedLocationRef = new Firebase(Constants.FIREBASE_URL_SEARCHED_LOCATION);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+        mFindRestaurants.setOnClickListener(this);
+
+        mSearchedLocationRefListener = mSearchedLocationRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick (View v) {
-                String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
-               Intent myIntent = new Intent(MainActivity.this, RestaurantListActivity.class);
-                myIntent.putExtra("location", location);
-                startActivity(myIntent);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String locations = dataSnapshot.getValue().toString();
+                Log.d("Location updated", locations);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
 
             }
         });
+
     }
-}
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSearchedLocationRef.removeEventListener(mSearchedLocationRefListener);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mFindRestaurants) {
+            String location = mLocationEditText.getText().toString();
+
+            saveLocationToFirebase(location);
+
+
+//            if(!(location).equals("")) {
+//                addToSharedPreferences(location);
+//            }
+//            Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
+//            startActivity(intent);
+        }
+    }
+
+            public void saveLocationToFirebase(String location) {
+            Firebase searchedLocationRef = new Firebase(Constants.FIREBASE_URL_SEARCHED_LOCATION);
+            searchedLocationRef.push().setValue(location);
+        }
+
+//    private void addToSharedPreferences(String location) {
+//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+    }
+//}
